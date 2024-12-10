b97b7c432bfeec9d4548ed658916bef173c37211
ayeshasikander <ayesha@724.one>
2024-12-10 15:04:53 +0500

    add header footer

--- app/layout.js
+++ app/layout.js
@@ -18,12 +18,24 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  let header = (
+    <header>
+      <h1 className="text-5xl text-red-600">Yum Recipe</h1>
+    </header>
+  );
+  let footer=(
+    <footer>
+      <p>@{new Date().getFullYear()} Yum Recipe_All rights reserved.</p>
+    </footer>
+  )
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {header}
         {children}
+        {footer}
       </body>
     </html>
   );
---
+++ app/recipes/[slug]/page.js
@@ -0,0 +1,10 @@
+const RecipePage = () => {
+  return (
+    <div>
+      <h1>Recipe Page</h1>
+      <p>This is a recipe page</p>
+      <p>Data will be here:</p>
+    </div>
+  );
+};
+export default RecipePage;
